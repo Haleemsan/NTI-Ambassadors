66b11f97419b690eea7d2c00873a85bf8bf8a88e
HaleemsAn175 <145738038+Haleemsan@users.noreply.github.com>
2024-10-11 02:55:36 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/* SHOW MENU */
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+
+
+
+// images slider
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const dots = document.querySelectorAll('.dot');
+
+let currentIndex = 0;
+
+function updateSliderPosition() {
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    updateActiveDot();
+}
+
+function updateActiveDot() {
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === currentIndex);
+    });
+}
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateSliderPosition();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateSliderPosition();
+});
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentIndex = index;
+        updateSliderPosition();
+    });
+});
